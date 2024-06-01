9eb6be80d843c7627c6398d1ab0f9b36a6c6b80c
[AdrianL] <[adrianlee061123@gmail.com]>
2024-06-01 22:16:47 +0800

    Jun1

--- src/components/Dialog_MainAdd/index.js
+++ src/components/Dialog_MainAdd/index.js
@@ -1,147 +1,156 @@
-// import { useState } from "react";
-// import {
-//   Button,
-//   InputLabel,
-//   MenuItem,
-//   Select,
-//   TextField,
-//   Typography,
-//   Dialog,
-//   DialogTitle,
-//   DialogContent,
-//   DialogActions,
-// } from "@mui/material";
-// import {
-//   Restaurant as RestaurantIcon,
-//   SportsEsports as SportsEsportsIcon,
-//   Favorite as FavoriteIcon,
-//   CardGiftcard as CardGiftcardIcon,
-//   EmojiTransportation as EmojiTransportationIcon,
-//   Pets as PetsIcon,
-//   ShoppingBag as ShoppingBagIcon,
-// } from "@mui/icons-material";
-// import { useCookies } from "react-cookie";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
-// import { addExpense } from "../../utils/api_expense";
-// import useCustomSnackbar from "../../components/useCustomSnackbar";
-// import { useState } from "react";
+import { useState } from "react";
+import {
+  Button,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
+import {
+  Restaurant as RestaurantIcon,
+  SportsEsports as SportsEsportsIcon,
+  Favorite as FavoriteIcon,
+  CardGiftcard as CardGiftcardIcon,
+  EmojiTransportation as EmojiTransportationIcon,
+  Pets as PetsIcon,
+  ShoppingBag as ShoppingBagIcon,
+} from "@mui/icons-material";
+import { useCookies } from "react-cookie";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addExpense } from "../../utils/api_expense";
+import useCustomSnackbar from "../../components/useCustomSnackbar";
 
-// export default function DialogMainAdd({
-//   openMainDialog,
-//   handleCloseMainDialog,
-// }) {
-//   const snackbar = useCustomSnackbar();
-//   const queryClient = useQueryClient();
-//   const [cookies] = useCookies(["currentUser"]);
-//   const { currentUser = {} } = cookies;
-//   const { role, token } = currentUser;
+export default function DialogMainAdd({
+  openMainDialog,
+  handleCloseMainDialog,
+}) {
+  const snackbar = useCustomSnackbar();
+  const queryClient = useQueryClient();
+  const [cookies] = useCookies(["currentUser"]);
+  const { currentUser = {} } = cookies;
+  const { token } = currentUser;
 
-//   const [name, setName] = useState("");
-//   const [amount, setAmount] = useState(0);
-//   const [category, setCategory] = useState("Category");
-//   const [description, setDescription] = useState("");
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("Category");
+  const [description, setDescription] = useState("");
 
-//   const addMainExpensesMutation = useMutation({
-//     mutationFn: addExpense,
-//     onSuccess: () => {
-//       snackbar.showSuccess("Income added.");
-//       queryClient.invalidateQueries({ queryKey: ["expenses"] });
-//       setName("");
-//       setCategory("Category");
-//       handleCloseMainDialog(true);
-//     },
-//     onError: (error) => {
-//       snackbar.showError(error.response.data.message);
-//     },
-//   });
+  const addMainExpensesMutation = useMutation({
+    mutationFn: addExpense,
+    onSuccess: () => {
+      snackbar.showSuccess("Expense added.");
+      queryClient.invalidateQueries({ queryKey: ["expenses"] });
+      setName("");
+      setAmount(0);
+      setCategory("Category");
+      setDescription("");
+      handleCloseMainDialog(false);
+    },
+    onError: (error) => {
+      snackbar.showError(error.response.data.message);
+    },
+  });
 
-//   const handleAddExpenses = () => {
-//     if (name === "" || description === "" || category === "Category") {
-//       snackbar.showWarning("Please fill in the details.");
-//     } else {
-//       addMainExpensesMutation.mutate({ name, description, category, token });
-//     }
-//   };
+  const handleAddExpenses = () => {
+    if (!name || !amount || !category) {
+      snackbar.showWarning("Please fill in the details.");
+    } else if (category === "Category") {
+      snackbar.showWarning("Please choose a Category.");
+    } else {
+      addMainExpensesMutation.mutate({
+        name,
+        amount,
+        description,
+        category,
+        token,
+      });
+    }
+  };
 
-//   return (
-//     <Dialog open={openMainDialog} onClose={handleCloseMainDialog}>
-//       <DialogTitle>What did you Spend Today ?</DialogTitle>
-//       <DialogContent>
-//         <TextField
-//           placeholder="Category name here..."
-//           variant="outlined"
-//           fullWidth
-//           value={name}
-//           onChange={(e) => setName(e.target.value)}
-//         />
-//         <TextField
-//           placeholder="Description"
-//           variant="outlined"
-//           fullWidth
-//           value={description}
-//           onChange={(e) => setDescription(e.target.value)}
-//         />
-//         <TextField
-//           placeholder="Amount"
-//           type="number"
-//           variant="outlined"
-//           fullWidth
-//           value={amount}
-//           onChange={(e) => setAmount(e.target.value)}
-//         />
-//         <Select
-//           labelId="icon-select-label"
-//           id="icon-select"
-//           fullWidth
-//           value={category}
-//           onChange={(e) => setCategory(e.target.value)}
-//           sx={{ marginTop: "10px" }}
-//         >
-//           <MenuItem value="Icons">
-//             <Typography>Icons</Typography>
-//           </MenuItem>
-//           <MenuItem value="Shopping">
-//             <ShoppingBagIcon />
-//             <Typography>Shopping</Typography>
-//           </MenuItem>
-//           <MenuItem value="Food">
-//             <RestaurantIcon />
-//             <Typography>Food</Typography>
-//           </MenuItem>
-//           <MenuItem value="Transportation">
-//             <EmojiTransportationIcon />
-//             <Typography>Transportation</Typography>
-//           </MenuItem>
-//           <MenuItem value="Entertainment">
-//             <SportsEsportsIcon />
-//             <Typography>Entertainment</Typography>
-//           </MenuItem>
-//           <MenuItem value="Pet">
-//             <PetsIcon />
-//             <Typography>Pet</Typography>
-//           </MenuItem>
-//           <MenuItem value="Health">
-//             <FavoriteIcon />
-//             <Typography>Health</Typography>
-//           </MenuItem>
-//           <MenuItem value="Gift">
-//             <CardGiftcardIcon />
-//             <Typography>Gift</Typography>
-//           </MenuItem>
-//         </Select>
-//       </DialogContent>
-//       <DialogActions sx={{ justifyContent: "center" }}>
-//         <Button
-//           variant="contained"
-//           color="warning"
-//           onClick={handleCloseMainDialog}
-//         >
-//           Cancel
-//         </Button>
-//         <Button variant="contained" color="warning" onClick={handleAddExpenses}>
-//           Add
-//         </Button>
-//       </DialogActions>
-//     </Dialog>
-//   );
-// }
+  return (
+    <Dialog open={openMainDialog} onClose={handleCloseMainDialog}>
+      <DialogTitle>What did you Spend Today?</DialogTitle>
+      <DialogContent>
+        <TextField
+          placeholder="Expenses"
+          variant="outlined"
+          fullWidth
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          placeholder="Description"
+          variant="outlined"
+          fullWidth
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          sx={{ marginTop: "10px", marginBottom: "10px" }}
+        />
+        <TextField
+          placeholder="Amount"
+          type="number"
+          variant="outlined"
+          fullWidth
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <Select
+          labelId="icon-select-label"
+          id="icon-select"
+          fullWidth
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          sx={{ marginTop: "10px" }}
+        >
+          <MenuItem value="Category">
+            <Typography>Category</Typography>
+          </MenuItem>
+          <MenuItem value="Shopping">
+            <ShoppingBagIcon />
+            <Typography>Shopping</Typography>
+          </MenuItem>
+          <MenuItem value="Food">
+            <RestaurantIcon />
+            <Typography>Food</Typography>
+          </MenuItem>
+          <MenuItem value="Transportation">
+            <EmojiTransportationIcon />
+            <Typography>Transportation</Typography>
+          </MenuItem>
+          <MenuItem value="Entertainment">
+            <SportsEsportsIcon />
+            <Typography>Entertainment</Typography>
+          </MenuItem>
+          <MenuItem value="Pet">
+            <PetsIcon />
+            <Typography>Pet</Typography>
+          </MenuItem>
+          <MenuItem value="Health">
+            <FavoriteIcon />
+            <Typography>Health</Typography>
+          </MenuItem>
+          <MenuItem value="Gift">
+            <CardGiftcardIcon />
+            <Typography>Gift</Typography>
+          </MenuItem>
+        </Select>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: "center" }}>
+        <Button
+          variant="contained"
+          color="warning"
+          onClick={handleCloseMainDialog}
+        >
+          Cancel
+        </Button>
+        <Button variant="contained" color="warning" onClick={handleAddExpenses}>
+          Add
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,10 @@
+import { useState } from "react";
 import { useCookies } from "react-cookie";
-import TopNav from "../../components/topNav";
-import BottomNav from "../../components/bottomNav";
-import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
-import RestaurantIcon from "@mui/icons-material/Restaurant";
-import EmojiTransportationIcon from "@mui/icons-material/EmojiTransportation";
-import PetsIcon from "@mui/icons-material/Pets";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   Button,
   Container,
-  Divider,
+  MenuItem,
   Table,
   TableBody,
   TableCell,
@@ -22,29 +13,31 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
+import EmojiTransportationIcon from "@mui/icons-material/EmojiTransportation";
+import PetsIcon from "@mui/icons-material/Pets";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import TopNav from "../../components/topNav";
+import BottomNav from "../../components/bottomNav";
 import useCustomSnackbar from "../../components/useCustomSnackbar";
 import { getExpenses } from "../../utils/api_expense";
 import { getCategories } from "../../utils/api_categories";
-import { useState } from "react";
+import DialogMainAdd from "../../components/Dialog_MainAdd";
+
 export default function Home() {
   const queryClient = useQueryClient();
   const snackbar = useCustomSnackbar();
-
-  //dialogExpenses
   const [openMainDialog, setOpenMainDialog] = useState(false);
-  const handleOpenMainDialog = () => {
-    setOpenMainDialog(true);
-  };
-
-  const handleCloseMainDialog = () => {
-    setOpenMainDialog(false);
-  };
-  //dialogExpenses
+  const handleOpenMainDialog = () => setOpenMainDialog(true);
+  const handleCloseMainDialog = () => setOpenMainDialog(false);
 
   const [cookies] = useCookies(["currentUser"]);
   const { currentUser = {} } = cookies;
-  const { role, token } = currentUser;
+  const { token } = currentUser;
 
   const { data: expenses = [] } = useQuery({
     queryKey: ["expenses", token],
@@ -56,30 +49,19 @@ export default function Home() {
     queryFn: () => getCategories(token),
   });
 
-  let todayDay = new Date();
-  let showDay = todayDay.getDay();
-  let answerDay = "";
-
-  if (showDay === 0) {
-    answerDay = "Sunday";
-  } else if (showDay === 1) {
-    answerDay = "Monday";
-  } else if (showDay === 2) {
-    answerDay = "Tuesday";
-  } else if (showDay === 3) {
-    answerDay = "Wednesday";
-  } else if (showDay === 4) {
-    answerDay = "Thursday";
-  } else if (showDay === 5) {
-    answerDay("Friday");
-  } else if (showDay === 6) {
-    answerDay = "Saturday";
-  }
-
-  const currentDate = new Date();
-  const currentMonth = currentDate.getMonth();
-
-  const monthNames = [
+  const today = new Date();
+  const currentDay = today.getDay();
+  const currentMonth = today.getMonth();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
     "January",
     "February",
     "March",
@@ -93,6 +75,8 @@ export default function Home() {
     "November",
     "December",
   ];
+  const currentDayName = days[currentDay];
+  const currentMonthName = months[currentMonth];
 
   const getIconComponent = (iconName) => {
     switch (iconName) {
@@ -119,6 +103,18 @@ export default function Home() {
     <>
       <TopNav />
       <Container style={{ paddingTop: "20px", width: "100%" }}>
+        <Button
+          endIcon={<AddCircleIcon />}
+          color="warning"
+          fullWidth
+          onClick={handleOpenMainDialog}
+        >
+          Add
+        </Button>
+        <DialogMainAdd
+          openMainDialog={openMainDialog}
+          handleCloseMainDialog={handleCloseMainDialog}
+        />
         <Container
           sx={{
             display: "flex",
@@ -129,39 +125,39 @@ export default function Home() {
           }}
         >
           {expenses.length > 0 ? (
-            expenses.map((e) => (
-              <TableContainer sx={{ maxWidth: "100%", width: "100%" }}>
-                <Table>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell width={"5%"} sx={{ color: "white" }}>
-                        {` ${monthNames[currentMonth]} `}
+            <TableContainer sx={{ maxWidth: "100%", width: "100%" }}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell width={"5%"} sx={{ color: "white" }}>
+                      {currentMonthName}
+                    </TableCell>
+                    <TableCell align="left" sx={{ color: "white" }}>
+                      {currentDayName}
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      sx={{ color: "white" }}
+                    ></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {expenses.map((expense) => (
+                    <TableRow key={expense._id}>
+                      <TableCell width={"20%"} sx={{ color: "white" }}>
+                        {getIconComponent(expense.category.name)}
                       </TableCell>
                       <TableCell align="left" sx={{ color: "white" }}>
-                        {`${answerDay}`}
+                        {expense.name}
                       </TableCell>
                       <TableCell align="right" sx={{ color: "white" }}>
-                        {/* {${expenses.income}} */}
+                        {expense.amount}
                       </TableCell>
                     </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    <TableRow key={e.id}>
-                      <TableCell width={"20%"} sx={{ color: "white" }}>
-                        {getIconComponent(e.icon)}
-                      </TableCell>
-                      <TableCell align="left" sx={{ color: "white" }}>
-                        {e.name}
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        sx={{ color: "white" }}
-                      ></TableCell>
-                    </TableRow>
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            ))
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           ) : (
             <Container
               sx={{
@@ -170,25 +166,9 @@ export default function Home() {
                 flexDirection: "column",
               }}
             >
-              <Typography
-                align="center"
-                variant="h4"
-                sx={{
-                  color: "white",
-                }}
-              >
+              <Typography align="center" variant="h4" sx={{ color: "white" }}>
                 No Expenses added yet.
               </Typography>
-              <Button
-                endIcon={<AddCircleIcon />}
-                color="warning"
-                fullWidth
-                onClick={() => {
-                  setOpenMainDialog(true);
-                }}
-              >
-                Add one now
-              </Button>
             </Container>
           )}
         </Container>
